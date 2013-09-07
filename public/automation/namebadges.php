b990eb9d997dbeef89c8743bc12b72e24ffed9bb
Andrew Betts <andrew@trib.tv>
2013-09-07 13:09:04 +0000

    Progress on name badges

--- public/automation/namebadges.php
+++ public/automation/namebadges.php
@@ -1,19 +1,21 @@
 <html>
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
-	<style>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+  	<style>
 	:root {-webkit-print-color-adjust:exact}
 	* { box-sizing: border-box; }
 	body, html { height: 100%; width: 100%; margin:0; padding:0; }
-	body { font-family: 'Open Sans', sans-serif; background: #f3f3f3; }
+	body { font-family: 'Lato', sans-serif; background: #f3f3f3; }
 	section { display: table; width: 850px; margin: 0 auto; background:white; height: 138mm; border-bottom: 1px dotted #ddd;}
 	section:nth-child(2n) { border-bottom: 1px dashed #ccc; }
 	section > div { display: table-cell; width: 50%; text-align: center; padding-top: 30mm; position: relative; border-left: 1px dotted #ddd; overflow:hidden; }
 	h1 { font-size: 60px; font-weight: normal; margin: 0; position: absolute; width: 100%; top: 30mm; }
 	h2 { margin-top:0; color: #555; font-weight: normal; font-size: 30px;position: absolute; width: 100%; top: 49mm;}
-	h3 { margin-top: 20mm; position: absolute; top: 57mm; width: 100%;}
+	h3 { margin-top: 20mm; position: absolute; top: 50mm; width: 100%;}
+	.footer { position: absolute; left:0; right:0; bottom:0; height: 170px; background: url(http://www.edgeconf.com/2013-nyc/images/bg_billboard.jpg); background-size: cover;}
+	.detail { list-style-type: none; position: absolute; bottom: 60px; left: 20px; padding:0; margin: 0; text-align: left; color: white; }
 
-	.logo { width: 140px; height: 62px; position: absolute; bottom: 25px; right: 25px;}
+	.logo { width: 140px; position: absolute; bottom: 35px; right: 25px;}
 	.bg { position: absolute; z-index: 0; top: 0; left: -27px; width: 479px; height: 546px; }
 
 	@media print {
@@ -21,166 +23,46 @@
 		section { border-bottom: none; width: 100%;}
 		section > div { border-left: none; }
 		section:nth-child(2n) { page-break-after: always; border-bottom: none; }
+		body { background-color: white; }
 	}
 	</style>
 </head>
 <body>
 <?php
 
-require_once '../app/libraries/http/HTTPRequest';
-require_once '../app/libraries/mysql/connection';
-require_once '../app/libraries/eventbrite/eventbrite.php';
-/*
-$eb_client = new Eventbrite(array('app_key'=>'N43KISGOIHOYSEYUDN', 'user_key'=>'133354408930710386593'));
+require_once '../../app/global';
 
-$list = $eb_client->event_list_attendees(array('id'=>4730469963));
+$eb_client = new Eventbrite(array('app_key'=>EVENTBRITE_APPKEY, 'user_key'=>EVENTBRITE_USERKEY));
+
+$sides = array('left', 'right');
+$list = $eb_client->event_list_attendees(array('id'=>EVENTBRITE_EID));
 foreach ($list->attendees as $rec) {
 	$company = isset($rec->attendee->company) ? $rec->attendee->company : '';
 	if (!$rec->attendee->first_name) continue;
-	if (strtotime($rec->attendee->created) < 1359244800) continue;
 	?>
 	<section>
-		<div class='left'>
-			<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-			<h1><?php echo $rec->attendee->first_name?></h1>
-			<h2><?php echo $rec->attendee->last_name?></h2>
-			<h3><?php echo $company?></h3>
-			<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-		</div>
-		<div class='right'>
-			<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-			<h1><?php echo $rec->attendee->first_name?></h1>
-			<h2><?php echo $rec->attendee->last_name?></h2>
-			<h3><?php echo $company?></h3>
-			<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-		</div>
+		<?php
+		foreach ($sides as $side) {
+			?>
+			<div class='<?php echo $side ?>'>
+				<h1><?php echo ucfirst($rec->attendee->first_name); ?></h1>
+				<h2><?php echo ucfirst($rec->attendee->last_name); ?></h2>
+				<h3><?php echo $company?></h3>
+				<div class='footer'>
+					<ul class='detail'>
+						<li>Edge conference</li>
+						<li>23 September 2013</li>
+						<li>Google</li>
+					</ul>
+					<img class='logo' src='http://www.edgeconf.com/2013-nyc/images/logo.png' />
+				</div>
+			</div>
+			<?php
+		}
+		?>
 	</section>
 	<?php
 }
-*/
 ?>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<h1>Shadow</h1>
-		<h2>(I'm a girl)</h2>
-		<h3>FT Labs</h3>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<h1>Shadow</h1>
-		<h2>(I'm a girl)</h2>
-		<h3>FT Labs</h3>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
-<section>
-	<div class='left'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-	<div class='right'>
-		<img src="http://edgeconf.com/img/herobg3.jpg" class="bg"/>
-		<img class='logo' src='http://edgeconf.com/img/logo_small.png' />
-	</div>
-</section>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- public/automation/namebadges_reverse.php
+++ public/automation/namebadges_reverse.php
@@ -24,6 +24,7 @@
 		section { border-bottom: none; width: 100%; }
 		section > div { border-left: none; }
 		section { page-break-after: always; border-bottom: none; }
+		body { background-color: white; }
 	}
 	</style>
 </head>
@@ -33,72 +34,8 @@
 			<h1>Welcome to Edge</h1>
 			<p>Edge is a unique conference format that invites you to participate in a structured discussion of some of the most important issues facing the web community today.  Each session will open with a 10 minute talk, then the moderator will guide a discussion of advanced topics and make sure we don't dwell on anything easily searchable online, so we can pack in as much useful content as possible.</p>
 			<p>This may mean you get lost occasionally if the topic is not familar, but Edge sessions are not intended to be introductions to a topic, so please bear with us!</p>
-			<p>The experience details you submitted in your registration have been flagged to relevant moderators, so don't be surprised if they seek you out looking for a contribution from you.</p>
-			<h3>Conference hub</h3>
-			<p>Everyone hates the one who gets up and asks a question (that turns out to be 'more of a comment really') in 17 parts, and jabbers away for hours.  Please don't be them.  To help us ask the questions everyone thinks are important, we are using Google Moderator.  To access it and other conference information, load the conference hub on your phone and laptop now:</p>
-			<code class='hublink'>edgeconf.com/hub</code>
-			<p>Most importantly, we hope you enjoy the conference!</p>
-		</div>
-		<div class='right'>
-		    <div class='schedule'>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T10:00:00Z'>10:00</time></div><h3>Offline</h3>
-				<p class='participants'>Jake Archibald (opener), Tobie Langel, Alex Russell, Mark Christian, Andrew Betts (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T11:00:00Z'>11:00</time></div><h3>Network</h3>
-				<p class='participants'>Ilya Grigorik (opener), Jackson Gabbard, John Cleveley, Andy Davies, Steve Thair (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T12:00:00Z'>12:00</time></div><h3>Break</h3>
-				<p class='participants'></p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T12:15:00Z'>12:15</time></div><h3>Performance</h3>
-				<p class='participants'>Shane O'Sullivan (opener), Rowan Beentje, Chris Lord, Pavel Feldman, Matt Delaney (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T13:15:00Z'>13:15</time></div><h3>Lunch</h3>
-				<p class='participants'></p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T14:15:00Z'>14:15</time></div><h3>Responsive layout</h3>
-				<p class='participants'>George Crawford (opener), Razvan Calliman, Tab Atkins, Andy Hume, Amber Weinberg (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T15:15:00Z'>15:15</time></div><h3>Input</h3>
-				<p class='participants'>Matt Caruana Galizia (opener), Boris Smus, Mairead Buchan, François Daoust, Pete LePage (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T16:15:00Z'>16:15</time></div><h3>Break</h3>
-				<p class='participants'></p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T16:30:00Z'>16:30</time></div><h3>Privileged access</h3>
-				<p class='participants'>Brian Leroux (opener), Paul Kinlan, Petro Soininen, Diana Cheng, Chris Heilmann (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T17:30:00Z'>17:30</time></div><h3>Testing and tooling</h3>
-				<p class='participants'>Paul Irish (opener), Remy Sharp, David Blooman, Simon Stewart, Ivan Zuzak (moderator)</p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T18:30:00Z'>18:30</time></div><h3>Closing remarks and thanks</h3>
-				<p class='participants'></p>
-			  </div>
-			  <div class='session'>
-			    <div class='session-time'><time datetime='2013-02-09T18:45:00Z'>18:45</time></div><h3>After party at the Crown</h3>
-				<p class='participants'>Directions available on the conference hub</p>
-			  </div>
-			</div>
-		</div>
-	</section>
-	<section>
-		<div class='left'>
-			<h1>Welcome to Edge</h1>
-			<p>Edge is a unique conference format that invites you to participate in a structured discussion of some of the most important issues facing the web community today.  Each session will open with a 10 minute talk, then the moderator will guide a discussion of advanced topics and make sure we don't dwell on anything easily searchable online, so we can pack in as much useful content as possible.</p>
-			<p>This may mean you get lost occasionally if the topic is not familar, but Edge sessions are not intended to be introductions to a topic, so please bear with us!</p>
-			<p>The experience details you submitted in your registration have been flagged to relevant moderators, so don't be surprised if they seek you out looking for a contribution from you.</p>
-			<h3>Conference hub</h3>
+			<p>The details you submitted in your registration have been flagged to relevant moderators, so don't be surprised if they seek you out looking for a contribution from you.</p>
+			<h3>OnSlyde interactive moderation</h3>
 			<p>Everyone hates the one who gets up and asks a question (that turns out to be 'more of a comment really') in 17 parts, and jabbers away for hours.  Please don't be them.  To help us ask the questions everyone thinks are important, we are using Google Moderator.  To access it and other conference information, load the conference hub on your phone and laptop now:</p>
 			<code class='hublink'>edgeconf.com/hub</code>
 			<p>Most importantly, we hope you enjoy the conference!</p>
@@ -158,20 +95,3 @@
 	</section>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
