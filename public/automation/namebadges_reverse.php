8b973d26ad2c9be732530dff03fe97bb13cdd771
Pete LePage <pete.lepage@pobox.com>
2013-09-18 19:22:39 -0400

    updated formatting for US paper sizes

--- public/automation/namebadges_reverse.php
+++ public/automation/namebadges_reverse.php
@@ -3,29 +3,28 @@
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
 	<style>
 	* { box-sizing: border-box; }
-	body, html { height: 100%; width: 100%; margin:0; padding:0; font-size: 11px }
-	p { margin: 0.7em 0;}
-	body { font-family: 'Open Sans', sans-serif; background: #f3f3f3; }
-	section { display: table; width: 850px; margin: 0 auto; background:white; height: 139mm; border-bottom: 1px dotted #ddd;}
-	section:nth-child(2n) { border-bottom: 1px dashed #ccc; }
-	section > div { display: table-cell; width: 50%; text-align: left; padding: 20px; position: relative; border-left: 1px dotted #ddd; vertical-align: top; }
-	h1 { font-size: 30px; font-weight: normal; margin: 0; }
-	h2 { font-weight: normal; font-size: 20px;}
-	h3 { margin: 1em 0 0 0; }
-	code.hublink { font-family: monospace; font-size: 25px; }
+  body, html { height: 100%; width: 100%; margin:0; padding:0; font-size: 10px }
+  p { margin: 0.7em 0;}
+  body { font-family: 'Open Sans', sans-serif; background: #f3f3f3; }
+  section { display: table; width: 850px; margin: 0 auto; background:white; height: 131mm; }
+  section > div { display: table-cell; width: 50%; text-align: left; padding: 20px; position: relative; border-left: 1px dotted #ddd; vertical-align: top; }
+  h1 { font-size: 30px; font-weight: normal; margin: 0; }
+  h2 { font-weight: normal; font-size: 20px;}
+  h3 { margin: 1em 0 0 0; font-size: 11px; }
+  code.hublink { font-family: monospace; font-size: 25px; }
 
-	.session { padding: 2px 0; border-top: 1px dotted #aaa; }
-	.session .session-time { width: 40px; margin:0; display: inline-block; }
-	.session h3 { display: inline-block; margin: 0;}
-	.session .participants { margin: 0 0 0 40px; font-size: 10px; color: #555; }
+  .session { padding: 2px 0; border-top: 1px dotted #aaa; }
+  .session .session-time { width: 40px; margin:0; display: inline-block; }
+  .session h3 { display: inline-block; margin: 0;}
+  .session .participants { margin: 0 0 0 40px; font-size: 9px; color: #555; }
 
-	@media print {
-		@page { size: 210mm 148mm; margin: 0; }
-		section { border-bottom: none; width: 100%; }
-		section > div { border-left: none; }
-		section { page-break-after: always; border-bottom: none; }
-		body { background-color: white; }
-	}
+  @media print {
+    @page { margin: 0; }
+    section { width: 100%; }
+    section > div { border-left: none; }
+    section:nth-child(2n) { page-break-after: always; }
+    body { background-color: white; }
+  }
 	</style>
 </head>
 <body>
@@ -93,5 +92,69 @@
 			</div>
 		</div>
 	</section>
+  <section>
+    <div class='left'>
+      <h1>Welcome to Edge</h1>
+      <p>Edge is a unique conference format that invites you to participate in a structured discussion of some of the most important issues facing the web community today.  Each session will open with a 10 minute talk, then the moderator will guide a discussion of advanced topics and make sure we don't dwell on anything easily searchable online, so we can pack in as much useful content as possible.</p>
+      <p>This may mean you get lost occasionally if the topic is not familar, but Edge sessions are not intended to be introductions to a topic, so please bear with us!</p>
+      <h3>Interactive moderation</h3>
+      <p>We're using an interactive moderation tool to allow everyone to get as involved in the debate as possible.  To get started, log onto the WiFi (GoogleGuest, no password) with your mobile device, and loas the short URL displayed in the top left of the big screen during the session.  Authenticate using your Google account, and you can then use the tool to request to speak during the debate, or to agree or disagree with the current speaker.  Please vote often!</p>
+      <p>For more important links and up to date on-the-day notices, see the conference hub:</p>
+      <code class='hublink'>edgeconf.com/hub</code>
+      <p>Most importantly, we hope you enjoy the conference!</p>
+    </div>
+    <div class='right'>
+        <div class='schedule'>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T10:00:00Z'>10:00</time></div><h3>Responsive images</h3>
+        <p class='participants'>Yoav Weiss (opener), Ann Robson, Peter Miller, Estelle Weyl, John Mellor, Marcos Caceres (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T11:00:00Z'>11:00</time></div><h3>Rendering performance</h3>
+        <p class='participants'>Jonathan Klein (opener), Paul Lewis, Ariya Hidayat, Joshua Peek, Andre Behrens (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T12:00:00Z'>12:00</time></div><h3>Break</h3>
+        <p class='participants'></p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T12:15:00Z'>12:15</time></div><h3>Third party scripts</h3>
+        <p class='participants'>Stoyan Stefanov (opener), Guy Podjarny, Ben Vinegar, Barbara Bermes, Steve Thair (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T13:15:00Z'>13:15</time></div><h3>Lunch</h3>
+        <p class='participants'></p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T14:15:00Z'>14:15</time></div><h3>Real time data</h3>
+        <p class='participants'>Henrik Joreteg (opener), Martyn Loughran, Wesley Hales, Rob Hawkes, John Fallows, Scott Jenson (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T15:15:00Z'>15:15</time></div><h3>Offline</h3>
+        <p class='participants'>Alex Russell (opener), Matt Andrews, Craig Cavalier, Calvin Spealman, Jake Archibald (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T16:15:00Z'>16:15</time></div><h3>Break</h3>
+        <p class='participants'></p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T16:30:00Z'>16:30</time></div><h3>Legacy clients</h3>
+        <p class='participants'>Tom Maslen (opener), Tomomi Imura, Shwetank Dixit, Edd Sowden, Paul Irish (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T17:30:00Z'>17:30</time></div><h3>Payments</h3>
+        <p class='participants'>Manu Sporney (opener), Ricardo Varela, Rob Grimshaw, Kumar McMillan, Cyndy Lobb, Natasha Rooney (moderator)</p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T18:30:00Z'>18:30</time></div><h3>Closing remarks and thanks</h3>
+        <p class='participants'></p>
+        </div>
+        <div class='session'>
+          <div class='session-time'><time datetime='2013-02-09T18:45:00Z'>19:00</time></div><h3>After party at The Park</h3>
+        <p class='participants'>Directions available on the conference hub</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </body>
 </html>
