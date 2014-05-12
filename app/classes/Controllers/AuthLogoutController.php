1a20fdb62ef210eccb0a05dc0f5f7c587282d50d
Andrew Betts <andrew.betts@ft.com>
2014-05-12 16:30:34 +0100

    Fix notice

--- app/classes/Controllers/AuthLogoutController.php
+++ app/classes/Controllers/AuthLogoutController.php
@@ -3,13 +3,13 @@
 namespace Controllers;
 
 class AuthLogoutController extends \Controllers\BaseController {
-	
+
 	public function get() {
-		
-		session_start();
+
+		@session_start();
 		$_SESSION = array();
 		session_destroy();
-		
+
 		$url = isset($_GET['redir']) ? $_GET['redir'] : '/';
 		$this->resp->redirect($url);
 	}
