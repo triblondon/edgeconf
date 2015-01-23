802d17d1a7f522820d3ad5b4a794847f802ec602
Andrew Betts <andrew.betts@ft.com>
2015-01-23 12:17:35 +0000

    Improve email MIME

--- app/classes/Controllers/AuthEmailSendCodeController.php
+++ app/classes/Controllers/AuthEmailSendCodeController.php
@@ -19,7 +19,12 @@ What?
 
 As part of the registration process on edgeconf.com (the website for the Edge web conference) we ask people to verify that they own the email address they are using to register.  Someone is asserting that they own ".$this->req->getPost('email').", so this email is intended to allow them to prove that they can receive mail sent to that address.  If you didn't register for Edge, you can ignore this email.";
 
-		mail($this->req->getPost('email'), 'Verify email for edgeconf.com', $body, "From: Edge <hello@edgeconf.com>");
+		$headers = "From: Edge <hello@edgeconf.com>\r\n";
+		$headers .= "Reply-To: Edge <hello@edgeconf.com>\r\n";
+		$headers .= "Return-Path: Edge <hello@edgeconf.com>\r\n";
+		$headers .= "Organization: London Web Performance\r\n";
+		$headers .= "Content-Type: text/plain\r\n";
+		mail($this->req->getPost('email'), 'Verify email for edgeconf.com', $body, $headers);
 
     }
 }
