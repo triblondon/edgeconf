06229bc06f4cc0bf03fa0745fde31d5dfb049c5e
Andrew Betts <andrew.betts@ft.com>
2015-01-26 15:37:23 +0000

    Use mailgun to send email, hopefully to increase deliverability

--- app/classes/Controllers/AuthEmailSendCodeController.php
+++ app/classes/Controllers/AuthEmailSendCodeController.php
@@ -8,23 +8,21 @@ class AuthEmailSendCodeController extends \Controllers\BaseController {
 
     	$code = substr(str_shuffle('QWERTYUIOPASDFGHJKLZXCVBNM1234567890'), 0, 9);
 
+    	// Stash it in the session
 		@session_start();
 		if (empty($_SESSION['emailverify'])) $_SESSION['emailverify'] = array();
 		$_SESSION['emailverify'][$this->req->getPost('email')] = $code;
 
-    	$body = "Your code is: $code
-
-What?
-=====
-
-As part of the registration process on edgeconf.com (the website for the Edge web conference) we ask people to verify that they own the email address they are using to register.  Someone is asserting that they own ".$this->req->getPost('email').", so this email is intended to allow them to prove that they can receive mail sent to that address.  If you didn't register for Edge, you can ignore this email.";
-
-		$headers = "From: Edge <hello@edgeconf.com>\r\n";
-		$headers .= "Reply-To: Edge <hello@edgeconf.com>\r\n";
-		$headers .= "Return-Path: Edge <hello@edgeconf.com>\r\n";
-		$headers .= "Organization: London Web Performance\r\n";
-		$headers .= "Content-Type: text/plain\r\n";
-		mail($this->req->getPost('email'), 'Verify email for edgeconf.com', $body, $headers);
-
+		// Send the email
+		$mg = new \Mailgun\Mailgun($this->app->config->mailgun->api_key);
+		$mg->sendMessage('edgeconf.com', array(
+			'from' => 'Edge conf <hello@edgeconf.com>',
+			'to' => $this->req->getPost('email'),
+			'subject' => 'Verify email using '.$code,
+			'text' => $this->app->view->render('emails/emailverify.txt', array(
+				'code' => $code,
+				'email' => $this->req->getPost('email')
+			))
+		));
     }
 }
