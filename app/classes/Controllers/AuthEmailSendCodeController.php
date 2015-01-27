4e59532b0a1375961d00d718c6267be646b79735
Andrew Betts <andrew.betts@ft.com>
2015-01-27 10:34:16 +0000

    Change to Sendgrid, malign is too slow.

--- app/classes/Controllers/AuthEmailSendCodeController.php
+++ app/classes/Controllers/AuthEmailSendCodeController.php
@@ -14,7 +14,7 @@ class AuthEmailSendCodeController extends \Controllers\BaseController {
 		$_SESSION['emailverify'][$this->req->getPost('email')] = $code;
 
 		// Send the email
-		$mg = new \Mailgun\Mailgun($this->app->config->mailgun->api_key);
+		/*$mg = new \Mailgun\Mailgun($this->app->config->mailgun->api_key);
 		$mg->sendMessage('edgeconf.com', array(
 			'from' => 'Edge conf <hello@edgeconf.com>',
 			'to' => $this->req->getPost('email'),
@@ -23,6 +23,21 @@ class AuthEmailSendCodeController extends \Controllers\BaseController {
 				'code' => $code,
 				'email' => $this->req->getPost('email')
 			))
-		));
+		));*/
+
+		$email = new \SendGrid\Email();
+		$email->addCategory('Email verify');
+		$email->addTo($this->req->getPost('email'));
+		$email->setFrom('hello@edgeconf.com');
+		$email->setFromName('Edge conf');
+		$email->setSubject('Verify email using '.$code);
+		$email->setText($this->app->view->render('emails/emailverify.txt', array(
+			'code' => $code,
+			'email' => $this->req->getPost('email')
+		)));
+
+		$sendgrid = new \SendGrid($this->app->config->sendgrid->username, $this->app->config->sendgrid->password);
+		$resp = $sendgrid->send($email);
+		var_dump($email, $sendgrid, $resp);
     }
 }
